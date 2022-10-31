26262b8f3e760da028ac378ebb75428f17e65c41
Lucas Santis <lucas.santis2@hotmail.com>
2022-10-31 14:36:32 -0300

    primera tentativa

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,17 +1,35 @@
 class Chronometer {
   constructor() {
+     this.currentTime = 0
+    this.intervalId = null
     // ... your code goes here
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+ if(printTimeCallback){
+  this.intervalId = setInterval( () =>{
+    this.currentTime = this.currentTime +1;
+    printTimeCallback();
+  } , 1000)
+ }else{
+  this.intervalId = setInterval(() => {
+    this.currentTime = this.currentTime +1;
+  }, 1000 );
+ }
+    
+  // ... your code goes here
   }
 
   getMinutes() {
+   let minutes = 0
+    minutes = currentTime / 60
+    return Math.floor(minutes)
     // ... your code goes here
   }
 
   getSeconds() {
+   let seconds = 0
+   seconds += currentTime % 60
     // ... your code goes here
   }
 
